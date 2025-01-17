2b86ac541947710d91d1884c2189dac135730c61
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2025-01-17 12:05:24 +0100

    added some notes about ORM

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -2,8 +2,19 @@ package pl.zajavka;
 
 public class Main {
     public static void main(String[] args) {
-        // ORM
-        // JPA
-        // HIBERNATE
+        // OOP - Object-Oriented-Programming
+        // ORM - Object-Relational-Mapping
+        // zalety - mniejsza ilosc kodu i przyspieszony proces tworzenia oprogramowania
+        // - DRY - eliminacja potrzeby powtarzania kodu
+        // - zwieksza bezpieczenstwo utrudniajac przeprowadzenie SQL Injection
+        //  mozna zmienic baze danych bez zmian w kodzie ( przykladowo z Postgres na Oracle)
+        // wady - gorsza wydajnosc dla skomplikowanych zapytan
+        // - utrata kontroli przez programiste nad wieloma rzeczami
+        // Eclipse Link, TopLink, Hibernate
+        // Entity - encja - w odniesieniu do POJO - klasaa uzywania w mapowaniu do bazy danych
+        // encja - klasa albo obiekt ktory bedzie uzuwany w mapowaniu pomiedzy java a baza danych
+        // JDBC - APi ktore umozliwia nam podstawowe narzedzia do polaczenia sie do bazy danych w Javie
+        
+
     }
 }
