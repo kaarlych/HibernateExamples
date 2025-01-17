46f5a6f07193218b35cefafbe3175bd3db1848dd
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2025-01-17 12:38:39 +0100

    added some notes about JPA

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -14,7 +14,23 @@ public class Main {
         // Entity - encja - w odniesieniu do POJO - klasaa uzywania w mapowaniu do bazy danych
         // encja - klasa albo obiekt ktory bedzie uzuwany w mapowaniu pomiedzy java a baza danych
         // JDBC - APi ktore umozliwia nam podstawowe narzedzia do polaczenia sie do bazy danych w Javie
-        
+
+        // JPA (Jakarta Persistance API) - standard okreslajacy w jaki sposob program java ma sie komunikowac
+        // z relacyjna baza danych
+        // specyfikacja ktora uwzglednia to jak obiekty w javie maja byc mapowane na tabele w bazie danych
+        // jakarta.persistance
+        // jak chcemy korzystac to trzeba dodac te interfejsy
+        // specyfikacja JPA - w jaki sposob mapwac biekty javowe na obiekty bazodanowe
+        // wspomaga pperacje CRUD - Create, Read, Update, Delete
+
+        // JPA vs JDBC
+        // najbardziej znanym vendorem JPA jest wlasnie Hibernate
+        // JPA - ustandaryzowana specyfikacja dla podejscia ORM
+        // dostawca JPA - jednym z nich jest Hibernate
+
+        // Persistance Unit - definiuje zestaw wsztstkich klas encji ktore maja byc zarzadzane przez manager
+        // mamy plik persistence.xml -
+
 
     }
 }
