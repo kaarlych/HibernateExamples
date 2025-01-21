0ab3da4dd258d7d761dda22e956a03a311e9b054
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2025-01-21 13:13:47 +0100

    added some notes about schemas in databases

--- src/main/java/pl/zajavka/Main.java
+++ src/main/java/pl/zajavka/Main.java
@@ -3,11 +3,21 @@ package pl.zajavka;
 public class Main {
     public static void main(String[] args) {
 
-        // transient, persistence, detached, removed
-        // Persistence Exception - paczka wyjatkow z jakarty
-        // TransactionRequiredException
-        // EntityNotFoundException
-        // QueryTimeoutException
+        // database objects:
+        // tabele
+        // constraints
+        // indeksy
+        // widoki
+
+        // database schema - w obrebie bazy danych mozna tworzyc schematy
+        // schemat w bazie danych jest przestrzenia, ktora zawiera tabele funkcje procedury etc
+        // select * from other.employee
+        // public jest schematem domyslnym
+        // schmaty pomagaja nam lepiej pogrupowac dane w logiczne grupy
+
+        // create schema company;
+        // create schema users;
+        // schemat sluzy do lepszej organizacji danych w bazie danych
 
     }
 }
